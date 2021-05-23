18dd5f040b945d8827cb95a0e8c75104e27731ca
Akshit Trikha <akshittrikha01@gmail.com>
2021-05-23 17:17:32 +0530

    upd: "Companie" Model upd: Add sgpa attribute in "StudentSchema" Schema fix: Syntax error in "JobOffer" Model

--- src/database/models/company.model.js
+++ src/database/models/company.model.js
@@ -2,87 +2,27 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CompanySchema = new Schema({
-    //Company
-    companyName: {
-        type: String,
-        required: true,
-    },
+	//Company
+	companyName: {
+		type: String,
+		required: true,
+	},
+	companyWebsite: {
+		type: String,
+	},
+	industrySector: {
+		type: String,
+	},
+	aboutCompany: {
+		type: String,
+	},
 
-    companyWebsite: {
-        type: String,
-    },
-    //     industrySector: {
-    //         type: String,
-    //     },
-    aboutCompany: {
-        type: String,
-    },
-
-    //! Added the following fields in the jobModel
-    //! Will be removed from here at a later stage of development
-    //Job Opening
-    jobProfile: {
-        type: String,
-        required: true,
-    },
-    passoutBatch: {
-        type: String,   //todo convert year
-        required: true
-    },
-
-    //whether intern or full-time placement
-    recruitmentType: {
-        type: String
-    },
-    duration: {
-        type: String
-    },
-    postingLocation: {
-        type: String
-    },
-    jobDescription: {
-        type: String
-    },
-    active: {
-        type: String,
-        default: true
-    },
-
-    //Student Eligibility
-    eligibility: {
-        type: Object
-    },
-    minCgpa: {
-        type: String   //todo convert float
-    },
-    maxBacklogsAllowed: {
-        type: String
-    },
-    //10th percent
-    //     minMetricPercent : {
-    //         type : String
-    //     },
-    //         //12th percent
-    //     minSeniorpercent : {
-    //         type : String
-    //     },
-
-    //Package
-    package: {
-        type: String
-    },
-
-    // registration deadline
-    deadlineDate: {
-        type: Date,
-        required: true
-    },
-
-    //No. of students applied
-    studentApplied: {
-        type: Schema.Types.ObjectId,
-        ref: "Student"
-    }
+	//TODO: Use array instead of String for one to many relations
+	//TODO: at a later stage of development
+	jobOpenings: {
+		type: Schema.Types.ObjectId,
+		ref: "JobOffer"
+	}
 });
 
 // create company model
