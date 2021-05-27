8e570fba2ebd8b041e0ab6255ed660a6cc8f77d4
darkphoenix17 <karanmistry284@gmail.com>
2021-05-27 19:06:42 +0530

    job/id/apply API created

--- src/routers/jobs.router.js
+++ src/routers/jobs.router.js
@@ -4,5 +4,6 @@ const jobs = require("../controllers/jobs/index");
 router.get("/all", jobs.all);
 router.get("/applied", jobs.applied);
 router.get("/:id", jobs.id);
+router.get("/:id/apply", jobs.apply);
 
 module.exports = router;
