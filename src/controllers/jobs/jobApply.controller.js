ae765c935701fec1d08cdd111f53c5b492373532
the-rdt <rdtiwari.official@gmail.com>
2021-05-30 11:43:16 +0530

    give notification to student on applying for job
    also add 'luxon' for better date formatting.
    run 'npm i' from root directory after pulling this commit

--- src/controllers/jobs/jobApply.controller.js
+++ src/controllers/jobs/jobApply.controller.js
@@ -1,24 +1,46 @@
 const JobModel = require("../../database/models/job.model");
 const StudentModel = require("../../database/models/student.model");
-const JobFilter = require("../../utils/jobsEachFilter");
+// const JobFilter = require("../../utils/jobsEachFilter");
 
-module.exports = async (req, res) => {
-    const jobId = req.params.id;
-    const studentId = req.user._id;
-    // console.log(studentId);
-    // console.log(jobId);
+const { DateTime } = require("luxon");
 
-    let student = await StudentModel.findById(studentId);
-    let job = await JobModel.findById(jobId);
+module.exports = async (req, res, next) => {
+  const jobId = req.params.id;
+  const studentId = req.user._id;
+  // console.log(studentId);
+  // console.log(jobId);
 
-    const found = student.jobApplied.find(x => x == jobId);
+  let student = await StudentModel.findById(studentId);
+  let job = await JobModel.findById(jobId).populate("company");
+
+  try {
+    const found = student.jobApplied.find((x) => x == jobId);
     if (!found) {
-        student.jobApplied.push(jobId);
-        job.studentsApplied.push(studentId);
+      await student.jobApplied.push(jobId);
+      const notification = {
+        iat: DateTime.now().toLocaleString(),
+        exp: DateTime.now().plus({ days: 10 }),
+        message: `You applied for Job at ${
+          job.company.companyName
+        } on ${DateTime.now().toLocaleString()}`,
+      };
+      await student.notification.push(notification);
+      await job.studentsApplied.push(studentId);
     }
 
     await StudentModel.findByIdAndUpdate(studentId, student);
     await JobModel.findByIdAndUpdate(jobId, job);
-    
-    res.send("OK");
+
+    res.send({ isApplied: "applied" });
+  } catch (err) {
+    const notification = {
+      iat: DateTime.now().toLocaleString(),
+      exp: DateTime.now().plus({ days: 10 }),
+      message: `You application process for Job at ${
+        job.company.companyName
+      } on ${DateTime.now().toLocaleString()} failed.`,
+    };
+    student.notification.push(notification);
+    next(err);
+  }
 };
