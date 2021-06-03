c8789c7684801df3ffe93237ee3544d5b50b676c
the-rdt <rdtiwari.official@gmail.com>
2021-06-03 10:13:52 +0530

    add isStudentEligible and isApplied property

--- src/controllers/jobs/jobsId.controller.js
+++ src/controllers/jobs/jobsId.controller.js
@@ -3,16 +3,23 @@ const StudentModel = require("../../database/models/student.model");
 const JobFilter = require("../../utils/jobsEachFilter");
 const CheckElig = require("../../utils/checkEligibility");
 
-
-module.exports = async (req, res) => {
+module.exports = async (req, res, next) => {
+  try {
     const jobId = req.params.id;
-    job = await JobModel.findById(jobId).populate("company");
+    job = (await JobModel.findById(jobId).populate("company")).toObject();
+    //   convert the data returned by mongoose to plain object to modify it further
     const student = await StudentModel.findById(req.user._id);
+    // console.log(student);
     let check = CheckElig(job, student);
-    Object.assign(job, check);
+    const isApplied = student.jobApplied.includes(jobId);
+    // console.log(check);
+    job.isStudentEligible = check.isElig;
+    job.isApplied = isApplied;
     // console.log(job);
     // let data = JobFilter(job);
-    // console.log(data);
     // console.log(job.isElig);
     res.send(job);
+  } catch (err) {
+    next(err);
+  }
 };
--- src/utils/checkEligibility.js
+++ src/utils/checkEligibility.js
@@ -1,20 +1,14 @@
 module.exports = (job, student) => {
-    let elig = student.approvedByTPO;
-    if(student.gender==='Male' && job.onlyForFemales) {
-        elig = false;
-    }
-    else if(student.backlogs > job.maxBacklogsAllowed) {
-        elig = false;
-    }
-    else {
-        for(let i=0; i<student.sgpa.length; i++) {
-            if(student.sgpa[i] < job.minCgpa) {
-                elig = false;
-                break;
-            }
-        }
-    }
-    return {
-        isElig: elig
-    }
-}
+  let elig = student.approvedByTPO;
+  if (student.gender === "Male" && job.onlyForFemales) {
+    elig = false;
+  } else if (student.backlogs > job.maxBacklogsAllowed) {
+    elig = false;
+  } else if (student.cgpa < job.minCgpa) {
+    elig = false;
+  }
+
+  return {
+    isElig: elig,
+  };
+};
