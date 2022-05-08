176b8ae961de3caebf7441b251cf51fd94ee10f6
therdt <rdtiwari.official@gmail.com>
2022-05-09 01:34:17 +0530

    pending job by id

--- src/controllers/tpo/index.js
+++ src/controllers/tpo/index.js
@@ -1,7 +1,8 @@
 module.exports = {
-    approvejob: require("./approveJob.controller"),
-    rejectjob: require("./rejectJob.controller"),
-    getpendingjob: require("./getPendingJob.controller"),
-    queries: require("./getQueries.controller"),
-    answerQuery: require("./answerQuery.controller"),
-  };
+  approvejob: require("./approveJob.controller"),
+  rejectjob: require("./rejectJob.controller"),
+  getpendingjob: require("./getPendingJob.controller"),
+  getpendingjobbyid: require("./getPendingJob.controller"),
+  queries: require("./getQueries.controller"),
+  answerQuery: require("./answerQuery.controller"),
+};
