1546de00267edb72a815d41aa35715048eeb3a8a
therdt <rdtiwari.official@gmail.com>
2022-05-18 18:10:36 +0530

    minor fixes

--- src/controllers/students/studentsAll.controller.js
+++ src/controllers/students/studentsAll.controller.js
@@ -2,7 +2,7 @@ const StudentModel = require("../../database/models/student.model");
 
 module.exports = async (req, res) => {
   // console.log(req);
-  const students = await StudentModel.find({})
-  .populate("jobApplied");
+  const students = await StudentModel.find({}).populate("jobApplied");
+  students.shift();
   res.send(students);
 };
